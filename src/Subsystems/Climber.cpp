d2f42c6cf0e32a6bd2193fc39fc3526703f4fb25
William Jin <wjin0702@yahoo.com>
2018-01-27 16:35:55 -0800

    Added climbing state machine, event handling and a notifier
    Change-Id: Ifc103d74061a74633eedb6ed0bc9ec32b4831ec9

---
+++ src/Subsystems/Climber.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2017-2018 FRC Team 3512. All Rights Reserved.
+
+#include "Climber.hpp"
+
+#include "Robot.hpp"
+
+enum class State { Init, Setup, Waiting, Climb, Idle };
+
+void Climber::HandleEvent(Event event) {
+    static State state = State::Idle;
+
+    switch (state) {
+        case State::Init:
+            if (event.type == EventType::kButtonPressed && event.param == 1) {
+                m_alignmentArms.Set(true);
+                Robot::intake.HandleEvent(EventType::kClimberSetup);
+                Robot::elevator.HandleEvent(EventType::kClimberSetup);
+                state = State::Setup;
+            }
+            break;
+        case State::Setup:
+            if (event.type == EventType::kAtSetHeight) {
+                m_setupSolenoid.Set(true);
+                state = State::Aligned;
+            }
+            break;
+        case State::Waiting:
+            if (event.type == EventType::kButtonPressed && event.param == 1) {
+                Robot::elevator.HandleEvent(EventType::kClimberClimb);
+                state = State::Climb;
+            }
+            break;
+        case State::Climb:
+            if (event.type == EventType::kAtSetHeight) {
+                state = State::Idle;
+            }
+            break;
+        case State::Idle:
+            break;
+    }
+}

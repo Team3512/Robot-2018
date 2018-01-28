18430961af967a78e0d4e1ea679e5c11f22b9513
William Jin <wjin0702@yahoo.com>
2018-01-27 16:35:55 -0800

    Added climbing state machine, event handling and a notifier
    Change-Id: Ifc103d74061a74633eedb6ed0bc9ec32b4831ec9

---
+++ src/Subsystems/Event.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2018 FRC Team 3512. All Rights Reserved.
+
+#pragma once
+
+#include <stdint.h>
+
+enum EventType {
+    kNoEvent,
+    kButtonPressed,
+    kClimberSetup,
+    kAtSetHeight,
+    kClimberClimb
+};
+
+struct Event {
+    Event(EventType type, int32_t param = 0);
+
+    EventType type = kNoEvent;
+    int32_t param = 0;
+};

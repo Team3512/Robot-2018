94732a7dc6c95034ca5762c597d80f5d03c87dcb
Charlie <charlesparkinson14@gmail.com>
2018-02-12 20:33:23 -0800

    Added unfinished Profiles and Pathfinder code
    *Created code to generate trajectories from waypoints
    *Made unfinished trajectory profile files
    *Fixed a deadlock in MotionProfile
    
    Change-Id: I79cf61be3dd52ccefd9cdc38096ce1ea40edad03

--- src/cpp/AutonomousModes/AutoRightPos.cpp
+++ src/cpp/AutonomousModes/AutoRightPos.cpp
@@ -26,10 +26,10 @@ void Robot::AutoRightPos() {
                 frc::DriverStation::GetInstance().GetGameSpecificMessage();
 
             if (gameData[0] == 'R') {
-                robotDrive.SetPositionReference(
+                robotDrive.SetPositionGoal(
                     168 - kRobotLength / 2);  // Back bumper to middle of robot
             } else {
-                robotDrive.SetPositionReference(228 - kRobotLength / 2);
+                robotDrive.SetPositionGoal(228 - kRobotLength / 2);
             }
             robotDrive.StartClosedLoop();
 
@@ -40,42 +40,42 @@ void Robot::AutoRightPos() {
             break;
 
         case State::kInitialForward:
-            if (robotDrive.PosAtReference() && autoTimer.HasPeriodPassed(1)) {
+            if (robotDrive.AtPositionGoal() && autoTimer.HasPeriodPassed(1)) {
                 if (gameData[0] == 'L') {
                     state = State::kFinalRotate;
                 } else {
                     robotDrive.ResetGyro();
-                    robotDrive.SetAngleReference(90);
+                    robotDrive.SetAngleGoal(90);
                     state = State::kRightForward;
                 }
             }
             break;
         case State::kRightRotate:
-            if (robotDrive.AngleAtReference() && autoTimer.HasPeriodPassed(1)) {
+            if (robotDrive.AtAngleGoal() && autoTimer.HasPeriodPassed(1)) {
                 robotDrive.ResetEncoders();
-                robotDrive.SetPositionReference(137);  // Estimate
+                robotDrive.SetPositionGoal(137);  // Estimate
                 state = State::kRightForward;
             }
             break;
         case State::kRightForward:
-            if (robotDrive.PosAtReference() && autoTimer.HasPeriodPassed(1)) {
+            if (robotDrive.AtPositionGoal() && autoTimer.HasPeriodPassed(1)) {
                 robotDrive.ResetEncoders();  // For Simplicity
 
                 robotDrive.ResetGyro();
-                robotDrive.SetAngleReference(90);
+                robotDrive.SetAngleGoal(90);
 
                 state = State::kFinalRotate;
             }
             break;
         case State::kFinalRotate:
-            if (robotDrive.AngleAtReference() && autoTimer.HasPeriodPassed(1)) {
+            if (robotDrive.AtAngleGoal() && autoTimer.HasPeriodPassed(1)) {
                 robotDrive.ResetEncoders();
-                robotDrive.SetPositionReference(20);  // Estimate
+                robotDrive.SetPositionGoal(20);  // Estimate
                 state = State::kFinalForward;
             }
             break;
         case State::kFinalForward:
-            if (robotDrive.PosAtReference() && autoTimer.HasPeriodPassed(1)) {
+            if (robotDrive.AtPositionGoal() && autoTimer.HasPeriodPassed(1)) {
                 robotDrive.StopClosedLoop();
                 state = State::kIdle;
             }

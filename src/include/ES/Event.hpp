00f825f886478644a908629b06a955d3e766d963
William Jin <wjin0702@yahoo.com>
2018-02-17 10:38:34 -0800

    Updated climber state machine so intake is stowed before elevator goes to a climb height, and the elevator resets to zero height even before that
    Added a prefixing system to the event naming conventions
    
    Change-Id: Ib43a7e150856cd5d4dec6e0aa8e374bbb9f1fb3a

--- src/include/ES/Event.hpp
+++ src/include/ES/Event.hpp
@@ -6,16 +6,24 @@
 
 /**
  * Event names to be polled and handled
+ *
+ * Event-Naming Prefix Key:
+ * 	no prefix indicates system-level events
+ * 	'Event' = user level events
+ * 	'Done' = reply messaging events
+ * 	'Cmd' = command events to another service
  */
 enum EventType {
     kNoEvent,
     kExit,
     kEntry,
-    kButtonPressed,
-    kAtSetHeight,
-    kElevatorSetSwitch,
-    kElevatorSetScale,
-    kElevatorSetClimb,
+    kEventButtonPressed,
+    kDoneAtSetHeight,
+    kCmdIntakeStow,
+    kCmdElevatorSetFloor,
+    kCmdElevatorSetSwitch,
+    kCmdElevatorSetScale,
+    kCmdElevatorSetClimb,
 };
 
 struct Event {

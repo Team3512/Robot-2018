9fce91ead6e5c8f42646ec50936ea0e8c24a3879
Charlie <charlesparkinson14@gmail.com>
2018-03-06 16:21:12 -0800

    Fixed unresponsiveness of GetRawButtonPressed()
    *Made Robot inherit from Service
    *Moved GetRawButtonPressed calls from Teleop to a new HandleEvent()
    *Made RobotPeriodic PostEvent() to Robot as well
    *Added an operator== to make getting button press events shorter
    Change-Id: Ia40baa815c094f75dd2a01f16c426cc8b58aa36c

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -72,9 +72,17 @@ void Robot::TestInit() {}
 void Robot::RobotPeriodic() {
     DS_PrintOut();
 
-    for (int i = 1; i < 12; i++) {
+    for (int i = 1; i <= 12; i++) {
         if (appendageStick.GetRawButtonPressed(i)) {
             Event event{EventType::kButtonPressed, i};
+            Robot::PostEvent(event);
+            climber.PostEvent(event);
+            elevator.PostEvent(event);
+            intake.PostEvent(event);
+        }
+        if (appendageStick.GetRawButtonReleased(i)) {
+            Event event{EventType::kButtonReleased, i};
+            Robot::PostEvent(event);
             climber.PostEvent(event);
             elevator.PostEvent(event);
             intake.PostEvent(event);
@@ -82,11 +90,7 @@ void Robot::RobotPeriodic() {
     }
 }
 
-void Robot::DisabledPeriodic() {
-    if (driveStick1.GetRawButtonPressed(12)) {
-        robotDrive.ResetEncoders();
-    }
-}
+void Robot::DisabledPeriodic() {}
 
 void Robot::AutonomousPeriodic() { dsDisplay.ExecAutonomousPeriodic(); }
 
@@ -100,46 +104,11 @@ void Robot::TeleopPeriodic() {
                          driveStick2.GetRawButton(2));
     }
 
-    // Intake Controls
-    if (appendageStick.GetRawButtonPressed(2)) {
-        climber.Shift();
-    }
-
-    if (appendageStick.GetRawButtonPressed(3)) {
-        if (intake.IsOpen()) {
-            intake.Close();
-        } else {
-            intake.Open();
-        }
-    }
-    if (appendageStick.GetRawButtonPressed(5) &&
-        !elevator.GetBottomHallEffect()) {
-        if (intake.IsDeployed()) {
-            intake.Stow();
-        } else {
-            intake.Deploy();
-        }
-    }
-    if (appendageStick.GetRawButtonPressed(4)) {
-        intake.SetMotors(MotorState::kIntake);
-    }
-    if (appendageStick.GetRawButtonPressed(6)) {
-        intake.SetMotors(MotorState::kOuttake);
-    }
-    if (appendageStick.GetRawButtonReleased(4) ||
-        appendageStick.GetRawButtonReleased(6)) {
-        intake.SetMotors(MotorState::kIdle);
-    }
-
     // Elevator Controls
-
     switch (elevatorMode) {
         case ElevatorMode::kPosition:  // TODO: change the Height
                                        // References back to constants when
                                        // we know the correct heights
-            if (!elevator.GetBottomHallEffect()) {
-                elevator.ResetEncoder();
-            }
             if (appendageStick.GetRawButton(7)) {
                 elevator.SetHeightReference(kFloorHeight);
             }
@@ -155,39 +124,11 @@ void Robot::TeleopPeriodic() {
             if (appendageStick.GetRawButton(11)) {
                 elevator.SetHeightReference(kClimbHeight);
             }
-            if (appendageStick.GetRawButtonPressed(5) &&
-                !elevator.GetBottomHallEffect()) {
-                if (intake.IsDeployed()) {
-                    intake.Stow();
-                } else {
-                    intake.Deploy();
-                }
-            }
-            if (appendageStick.GetRawButtonPressed(12)) {
-                elevator.SetHeightReference(elevator.GetHeight());
-                elevator.StopClosedLoop();
-                elevatorMode = ElevatorMode::kVelocity;
-            }
             break;
         case ElevatorMode::kVelocity:
             elevator.SetVelocity(appendageStick.GetY());
-            if (!elevator.GetBottomHallEffect()) {
-                elevator.ResetEncoder();
-            }
-            if (appendageStick.GetRawButtonPressed(12)) {
-                elevator.SetHeightReference(elevator.GetHeight());
-                elevator.StartClosedLoop();
-                elevatorMode = ElevatorMode::kPosition;
-            }
+            break;
     }
-
-    /*if (appendageStick.GetRawButtonPressed(11)) {
-        if (server.GetSource() == camera1) {
-            server.SetSource(camera2);
-        } else {
-            server.SetSource(camera1);
-        }
-    }*/
 }
 
 void Robot::DS_PrintOut() {
@@ -205,6 +146,77 @@ void Robot::DS_PrintOut() {
         liveGrapher.GraphData(robotDrive.GetPosition(), "Position");
         liveGrapher.ResetInterval();
     }
+}
+
+void Robot::HandleEvent(Event event) {
+    // Intake Controls
+    if (event == Event{kButtonPressed, 2}) {
+        climber.Shift();
+    }
+
+    if (event == Event{kButtonPressed, 3}) {
+        if (intake.IsOpen()) {
+            intake.Close();
+        } else {
+            intake.Open();
+        }
+    }
+    if (event == Event{kButtonPressed, 5} && !elevator.GetBottomHallEffect()) {
+        if (intake.IsDeployed()) {
+            intake.Stow();
+        } else {
+            intake.Deploy();
+        }
+    }
+    if (event == Event{kButtonPressed, 4}) {
+        intake.SetMotors(MotorState::kIntake);
+    }
+    if (event == Event{kButtonPressed, 6}) {
+        intake.SetMotors(MotorState::kOuttake);
+    }
+    if (event == Event{kButtonPressed, 4} ||
+        event == Event{kButtonPressed, 6}) {
+        intake.SetMotors(MotorState::kIdle);
+    }
+
+    // Elevator Controls
+    switch (elevatorMode) {
+        case ElevatorMode::kPosition:  // TODO: change the Height
+                                       // References back to constants when
+                                       // we know the correct heights
+            if (event == Event{kButtonPressed, 5} &&
+                !elevator.GetBottomHallEffect()) {
+                if (intake.IsDeployed()) {
+                    intake.Stow();
+                } else {
+                    intake.Deploy();
+                }
+            }
+            if (event == Event{kButtonPressed, 12}) {
+                elevator.SetHeightReference(elevator.GetHeight());
+                elevator.StopClosedLoop();
+                elevatorMode = ElevatorMode::kVelocity;
+            }
+            break;
+        case ElevatorMode::kVelocity:
+            if (!elevator.GetBottomHallEffect()) {
+                elevator.ResetEncoder();
+            }
+            if (event == Event{kButtonPressed, 12}) {
+                elevator.SetHeightReference(elevator.GetHeight());
+                elevator.StartClosedLoop();
+                elevatorMode = ElevatorMode::kPosition;
+            }
+            break;
+    }
+
+    /*if (event == Event{kButtonPressed, 11}) {
+        if (server.GetSource() == camera1) {
+            server.SetSource(camera2);
+        } else {
+            server.SetSource(camera1);
+        }
+    }*/
 }
 
 START_ROBOT_CLASS(Robot)

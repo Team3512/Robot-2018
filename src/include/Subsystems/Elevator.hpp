502c0eabee5a3feb6bc172c5e62974e8ee6cc3f7
William Jin <wjin0702@yahoo.com>
2018-03-10 17:38:01 -0800

    Added unfinished elevator encoder safety checking
    Change-Id: Id66ce6de51ef92c747d9d6183f6557c365a85add

--- src/include/Subsystems/Elevator.hpp
+++ src/include/Subsystems/Elevator.hpp
@@ -9,6 +9,7 @@
 #include <CtrlSys/SumNode.h>
 #include <DigitalInput.h>
 #include <Notifier.h>
+#include <RobotController.h>
 #include <ctre/phoenix/MotorControl/CAN/WPI_TalonSRX.h>
 
 #include "Constants.hpp"
@@ -27,6 +28,8 @@ public:
     // Sets the voltage of the motors
     void SetVelocity(double velocity);
 
+    double GetVelocity() const;
+
     // Set encoder distance to 0
     void ResetEncoder();
 
@@ -43,6 +46,10 @@ public:
     // Returns encoder PID loop references
     double GetHeightReference() const;
 
+    double GetAcceleration(double voltage) const;
+
+    void CheckEncoderSafety(double joystick_value);
+
     // Returns whether or not elevator has reached reference
     bool HeightAtReference() const;
 
@@ -56,10 +63,13 @@ private:
     WPI_TalonSRX m_elevatorSlaveMotor{kElevatorSlaveID};
     CANTalonGroup m_elevatorGearbox{m_elevatorMasterMotor,
                                     m_elevatorSlaveMotor};
+    double m_simulatedVelocity = 0.0;
 
     Notifier m_notifier;
     // Reference
     frc::RefInput m_heightRef{0.0};
+
+    Timer m_timer;
 
     // Sensors
     frc::DigitalInput m_elevatorBottomHall{kElevatorBottomHallPort};

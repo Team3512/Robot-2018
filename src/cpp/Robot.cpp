bce62023fb0dce934865286bd90ac151fd99a9dc
Dwopple <charlesparkinson14@gmail.com>
2018-03-27 19:16:08 -0700

    Setup for googletest
    Change-Id: Ib08ba72a6af131c3e1a5c5b3d09a8eb71e1a3216

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -7,6 +7,10 @@
 
 #include <DriverStation.h>
 
+#ifdef TEST
+#include "gtest/gtest.h"
+#endif
+
 DriveTrain Robot::robotDrive;
 Intake Robot::intake;
 Elevator Robot::elevator;
@@ -198,4 +202,11 @@ void Robot::DS_PrintOut() {
     // successful(butchered) upload
 }
 
+#ifdef TEST
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#else
 START_ROBOT_CLASS(Robot)
+#endif
---
+++ test/src/main/native/cpp/ESTest.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 FRC Team 3512. All Rights Reserved.
+
+#include "ESTest.hpp"
+
+#include "ES/Service.hpp"
+
+class ESTest : public testing::Test {
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(ESTest, TestTest) { EXPECT_TRUE(true); }

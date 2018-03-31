6afa5078858671fcfe4eb64820194718f61053fb
Dwopple <charlesparkinson14@gmail.com>
2018-03-31 08:40:47 -0700

    Changed AutoAutoLineTimed
    *6 seconds long, half speed, forward
    
    Change-Id: Ife9b98d199e98bec909162943e7afb5b3508139b

--- src/cpp/AutonomousModes/AutoAutoLineTimed.cpp
+++ src/cpp/AutonomousModes/AutoAutoLineTimed.cpp
@@ -7,7 +7,7 @@ void Robot::AutoAutoLineTimedInit() {}
 // Incase nothing is working and we want to attempt a ranking point by getting
 // past the autoline
 void Robot::AutoAutoLineTimedPeriodic() {
-    if (autoTimer.Get() < 7.0) {  // Estimate
+    if (autoTimer.Get() < 6.0) {  // Estimate
         robotDrive.Drive(0.5, 0.0, false);
     } else {
         robotDrive.Drive(0.0, 0.0, false);
